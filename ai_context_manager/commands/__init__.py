f6fa12d04ca71b21b67af8d33060facb263c096c
Marc Christenfeldt <marc.christenfeldt@gmx.de>
2025-09-11 22:28:31 +0200

    chore: Remove commands package init - Removes the now-empty `commands/__init__.py` file.

--- ai_context_manager/commands/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Commands package for AI Context Manager."""
-
-from ai_context_manager.commands.export_cmd import app as export_app
-from ai_context_manager.commands.profile_cmd import app as profile_app
-
-__all__ = ["export_app", "profile_app"]
